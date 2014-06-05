0fb48bc7bcfda109f76fcb9cdfb89a602de2d2fd
Maximiliano Hernández <kotick@MacBook-Pro-de-Maximiliano.local>
2014-06-05 18:09:07 -0400

    Entrada funcionando

--- lab1.cpp
+++ lab1.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 
 using namespace std;
 
-int main(int argc, char const *argv[]){
-	string c;
+int main(int argc, char **argv){
+	char c;
 	string entrada, salida, archivolog;
-	while ((c = getopt (argc, argv, "ai:o:")) != -1){
+	while ((c = getopt (argc, argv, "i:o:l:")) != -1){
 		switch (c){
 			case 'i':
 				entrada = optarg;
 				break;
 			case 'o':
-				salid = optarg;
+				salida = optarg;
 				break;
 			case 'l':
 				archivolog = optarg;
 				break;
 			default:
-				cout <<error<<endl;
+				cout << "error" <<endl;
+				break;
 		}
 	}
 	return 0;
