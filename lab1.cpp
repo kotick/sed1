46660c7906eaee0e56362eabe2a530c42ba49042
Maximiliano Hernández <kotick@MacBook-Pro-de-Maximiliano.local>
2014-06-05 17:44:57 -0400

    Lectura de parámetros de entrada.

--- lab1.cpp
+++ lab1.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main(int argc, char const *argv[]){
+	string c;
+	string entrada, salida, archivolog;
+	while ((c = getopt (argc, argv, "ai:o:")) != -1){
+		switch (c){
+			case 'i':
+				entrada = optarg;
+				break;
+			case 'o':
+				salid = optarg;
+				break;
+			case 'l':
+				archivolog = optarg;
+				break;
+			default:
+				cout <<error<<endl;
+		}
+	}
 	return 0;
 }
